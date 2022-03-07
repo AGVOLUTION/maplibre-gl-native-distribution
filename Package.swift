8a45d8d41d973c46726990de545e2508819e790c
Sebastian Jerratsch <60115651+sjerratsch@users.noreply.github.com>
2022-03-07 10:40:19 +0100

    update to 5.12.2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mapbox",
-            url: "https://github.com/AGVOLUTION/maplibre-gl-native-distribution/releases/download/5.11.1/Mapbox-5.11.1.zip",
-            checksum: "6ce691a4a5eee3eefbcbc2b4792252628533922eb70b18cbbad5bfeb073bea4d")
+            url: "https://github.com/AGVOLUTION/maplibre-gl-native-distribution/releases/download/5.12.2/Mapbox-5.12.2.zip",
+            checksum: "0d6b73f9f9a2550e084f0f6702d2e257a59128c7f0d6cf05b0f2dd391447c4d4")
     ]
 )
